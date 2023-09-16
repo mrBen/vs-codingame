2f58155f4132e403598df17f975eb8efb74b6665
Benjamin Collet <benjamin.collet@protonmail.ch>
2023-09-16 10:46:46 +0200

    Add commands

--- src/extension.ts
+++ src/extension.ts
@@ -6,10 +6,15 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "vs-codingame" is now active!');
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('vs-codingame.openCodingame', () => {
-			vscode.env.openExternal(vscode.Uri.parse('https://www.codingame.com/home'));
-		})
+		vscode.commands.registerCommand('vs-codingame.openCodingame', openCodingame)
 	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('vs-codingame.openSettings', openSettings)
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('vs-codingame.changeFile', chooseFile)
+	);
+
 	vscode.window.registerTreeDataProvider(
 		'cgSynchro',
 		new synchro.SyncedProvider()
@@ -17,3 +22,22 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() { }
+
+function openCodingame() {
+	vscode.env.openExternal(vscode.Uri.parse('https://www.codingame.com/home'));
+}
+
+function openSettings() {
+	vscode.commands.executeCommand('workbench.action.openSettings', 'VS CodinGame');
+}
+
+export async function chooseFile() {
+	console.log('begin choosing a file');
+
+	let file = await vscode.window.showOpenDialog({
+		defaultUri: vscode.Uri.parse('/home/benjamin/Projects/vs-codingame/'),
+		title: 'Hop'
+	});
+
+	console.log(file);
+}
